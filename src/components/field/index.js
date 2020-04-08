c3dc40d00c9272393d8911193d8079157ff1addd
xvonabur <webdev.xvonabur@gmail.com>
2020-04-08 16:55:16 +0300

    Fix deprecation warnings

--- src/components/field/index.js
+++ src/components/field/index.js
@@ -128,7 +128,7 @@ export default class TextField extends PureComponent {
     };
   }
 
-  componentWillReceiveProps(props) {
+  UNSAFE_componentWillReceiveProps(props) {
     let { error } = this.state;
 
     if (null != props.value) {
@@ -152,7 +152,7 @@ export default class TextField extends PureComponent {
     this.mounted = false;
   }
 
-  componentWillUpdate(props, state) {
+  UNSAFE_componentWillUpdate(props, state) {
     let { error, animationDuration: duration } = this.props;
     let { focus, focused } = this.state;
 
